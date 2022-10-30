499c198ec8f08a799ef5e6748de25ced9657b360
joelmunu <joel.munu@gmail.com>
2022-10-30 11:54:40 +0000

    Feature(ProductInput): Terminado el apartado visual de Product Input

--- components/ProductInput.jsx
+++ components/ProductInput.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
 
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Pressable, StyleSheet, TextInput, Text, View } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+
+const productType = [
+    'Fruit',
+    'Vegetable',
+    'Bakery',
+    'Fish',
+    'Meat'
+]
 
 const ProductInput = ({ onProductAdd }) => {
     const [productName, setProductName] = useState('');
@@ -19,36 +28,111 @@ const ProductInput = ({ onProductAdd }) => {
 
     return (
         <View style={styles.productInput}>
-            <TextInput style={styles.productName}
-                placeholder='Introduzca un producto'
-                keyboardType="default"
-                onChangeText={changeTextHandler}
-                value={productName} />
-            <Button
-                style={styles.addButton}
-                title="Añadir"
-                onPress={addProductHandler} />
+            <View style={styles.View1}>
+                <TextInput style={styles.productName}
+                    placeholder='Product name...'
+                    keyboardType="default"
+                    onChangeText={changeTextHandler}
+                    value={productName} />
+
+                <TextInput style={styles.productQuantity}
+                    placeholder='Quantity...'
+                    keyboardType='Numeric'
+
+                />
+            </View>
+            <View style={styles.View2}>
+                <SelectDropdown
+                    data={productType}
+                    defaultButtonText={'Product type...'}
+                    buttonStyle={styles.dropdownButton}
+                    buttonTextStyle={styles.dropdownBtnText}
+                    rowTextStyle={styles.dropdown}
+                />
+                <View style={styles.pressable}>
+                    <Pressable
+                        style={styles.addButton}
+                        onPress={addProductHandler}>
+                        <Text style={styles.btnText}>Add</Text>
+                    </Pressable>
+                </View>
+
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     productInput: {
-        flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: "#ad1457",
-        width: '80%',
-        height: 80,
+        backgroundColor: "lemonchiffon",
+        marginTop: 10,
+        width: '95%',
+        height: 140,
         borderRadius: 5,
         padding: 10
     },
-    productName: {
-        flex: 4,
-        color: 'white'
+
+    View1: {
+        flexDirection: 'row'
     },
+
+    productName: {
+        flex: 4.1,
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        padding: 5,
+        marginRight: 10,
+        marginLeft: 10,
+        borderRadius: 5
+    },
+
+    productQuantity: {
+        flex: 2,
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        padding: 5,
+        marginRight: 10,
+        borderRadius: 5
+    },
+
+    View2: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    dropdownButton: {
+        flex: 3,
+        backgroundColor: 'lemonchiffon',
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        borderRadius: 5,
+        height: 40,
+        marginRight: 10,
+    },
+
+    dropdownBtnText: {
+        fontSize: 16
+    },
+
     addButton: {
-        flex: 1
+        backgroundColor: '#ffff8f',
+        marginRight: 10,
+        marginLeft: 10,
+        paddingTop: 12,
+        paddingBottom: 12,
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        borderRadius: 5
+    },
+
+    btnText: {
+        textAlign: 'center'
+    },
+
+    pressable: {
+        flex: 2
     }
 });
 
